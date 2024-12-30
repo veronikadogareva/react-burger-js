825aa5c36af69941158db8c1da052be6565d6c81
Bogdan3535 <bogdantarasov14@gmail.com>
2024-12-30 15:01:51 +0600

    review 1-2

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -10,7 +10,7 @@ import {
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import OrderDetails from "../OrderDetails/OrderDetails";
 import Modal from "../Modal/Modal";
-import IngredientType from '../../utils/types';
+import IngredientType from "../../utils/types";
 
 export default function BurgerConstructor({ ingredients }) {
   const [isModal, setIsModal] = useState(false);
@@ -21,81 +21,19 @@ export default function BurgerConstructor({ ingredients }) {
       <section className={burgerConstructorStyles.container}>
         <SimpleBar style={{ maxHeight: 656 }}>
           <ul>
-            <li>
-              <ConstructorElement
-                type="top"
-                isLocked={true}
-                text="Краторная булка N-200i (верх)"
-                price={200}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
-            <li>
-              <DragIcon type="primary" />
-              <ConstructorElement
-                text="Краторная булка N-200i (верх)"
-                price={50}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
-            <li>
-              <DragIcon type="primary" />
-              <ConstructorElement
-                text="Краторная булка N-200i (верх)"
-                price={50}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
-            <li>
-              <DragIcon type="primary" />
-              <ConstructorElement
-                text="Краторная булка N-200i (верх)"
-                price={50}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
-            <li>
-              <DragIcon type="primary" />
-              <ConstructorElement
-                text="Краторная булка N-200i (верх)"
-                price={50}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
-            <li>
-              <DragIcon type="primary" />
-              <ConstructorElement
-                text="Краторная булка N-200i (верх)"
-                price={50}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
-            <li>
-              <DragIcon type="primary" />
-              <ConstructorElement
-                text="Краторная булка N-200i (верх)"
-                price={50}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
-            <li>
-              <DragIcon type="primary" />
-              <ConstructorElement
-                text="Краторная булка N-200i (верх)"
-                price={50}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
-
-            <li>
-              <ConstructorElement
-                type="bottom"
-                isLocked={true}
-                text="Краторная булка N-200i (низ)"
-                price={200}
-                thumbnail="https://code.s3.yandex.net/react/code/cheese.png"
-              />
-            </li>
+            {ingredients.map((ingredient) => {
+              return (
+                <li key={ingredient._id}>
+                  <ConstructorElement
+                    type={ingredient.type}
+                    isLocked={true}
+                    text={ingredient.name}
+                    price={ingredient.price}
+                    thumbnail={ingredient.image_mobile}
+                  />
+                </li>
+              );
+            })}
           </ul>
         </SimpleBar>
         <div className={burgerConstructorStyles.total}>
